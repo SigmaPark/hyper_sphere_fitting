bc7543906aa990a046dbfbbc3569b5de62da56a0
Sigma Park <greengb@naver.com>
2024-02-12 02:20:10 +0900

    feat: optimize Sumith YD algorithm

--- Sumith_YD_Fit_Sphere.hpp
+++ Sumith_YD_Fit_Sphere.hpp
@@ -26,23 +26,23 @@ auto prac::Sumith_YD_fit_sphere(CON const& con)-> sgm::Array<T, D+1>
 {
 	using std::size_t;
 
-	auto diag_f
-	= [](s3d::Matrix<T, D, D> const& M)-> s3d::Vector<T, D>
+	auto trace_f
+	= [](s3d::Matrix<T, D, D> const& M) noexcept-> T
 	{
-		s3d::Vector<T, D> res;
+		auto res = T(0);
 
 		for(size_t i = 0;  i < D;  ++i)
-			res(i) = M(i, i);
+			res += M(i, i);
 
 		return res;
 	};
 
 	auto const [N, S1, S2, S3]
-	= [diag_f, &con]
+	= [trace_f, &con]
 	{
 		size_t N = 0;
-		s3d::Vector<T, D> S1 = decltype(S1)::Zero();
-		s3d::Matrix<T, D, D> S2 = decltype(S2)::Zero(), S3 = S2;
+		s3d::Vector<T, D> S1 = decltype(S1)::Zero(), S3 = S1;
+		s3d::Matrix<T, D, D> S2 = decltype(S2)::Zero();
 
 		for(auto const& p : con)
 		{
@@ -52,7 +52,7 @@ auto prac::Sumith_YD_fit_sphere(CON const& con)-> sgm::Array<T, D+1>
 			++N;
 			S1 += v;
 			S2 += uvdv;
-			S3 += diag_f(uvdv).dyadic(v);
+			S3 += trace_f(uvdv)*v;
 		}
 
 		mat_op_helper::Copy_upper_to_lower<T, D>(S2);
@@ -60,31 +60,10 @@ auto prac::Sumith_YD_fit_sphere(CON const& con)-> sgm::Array<T, D+1>
 		return sgm::Make_Family(N, S1, S2, S3);
 	}();
 
-	auto const tr_S2
-	= [&S2=S2]
-	{
-		T res = 0;
-
-		for(size_t k = 0;  k < D;  ++k)
-			res += S2(k, k);
-
-		return res;
-	}();
-
-	auto const S3_colsum
-	= [&S3=S3]
-	{
-		s3d::Vector<T, D> res = decltype(res)::Zero();
-
-		for(size_t i = 0;  i < D;  ++i)
-			for(size_t j = 0;  j < D;  ++j)
-				res(j) += S3(i, j);
-
-		return res;
-	}();
+	auto const tr_S2 = trace_f(S2);
 
 	s3d::Matrix<T, D, D> const A = 2*( mat_op_helper::Self_dyadic<T, D>(S1) - N*S2 );
-	s3d::Vector<T, D> const b = tr_S2*S1 - N*S3_colsum;
+	s3d::Vector<T, D> const b = tr_S2*S1 - N*S3;
 	
 	s3d::Vector<T, D> const c = A.inv()*b;
 	T const r = std::sqrt(  ( tr_S2 - 2*c.dot(S1) )/N + c.sqr_norm()  );
